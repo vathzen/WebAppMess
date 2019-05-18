159a9f739bf32876c454e25bd6e476d6be11a29c
Vathsan <srivaths311@gmail.com>
2019-05-18 17:57:28 +0530

    Menu now updates Server

---
+++ src/app/services/classes.ts
@@ -0,0 +1,37 @@
+export class Response {
+    Status: string;
+    Text: string;
+    constructor(values: Object = {}){
+        Object.assign(this, values);
+    }
+}
+
+export class Menu {
+    bf1: string;
+    bf1c: string;
+    bf2: string;
+    bf2c: string;
+    lun1: string;
+    lun1c: string;
+    lun2: string;
+    lun2c: string;
+    din1: string;
+    din1c: string;
+    din2: string;
+    din2c: string;
+
+    constructor(array: string[]){
+        this.bf1 = array[0];
+        this.bf1c = array[1];
+        this.bf2 = array[2];
+        this.bf2c = array[3];
+        this.lun1 = array[4];
+        this.lun1c = array[5];
+        this.lun2 = array[6];
+        this.lun2c = array[7];
+        this.din1 = array[8];
+        this.din1c = array[9];
+        this.din2 = array[10];
+        this.din2c = array[11];
+    }
+}
---
+++ src/app/services/rest.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpErrorResponse } from '@angular/common/http';
+import { Response } from './classes';
+import { Observable,throwError } from 'rxjs';
+import { catchError,map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RestService {
+
+  baseUrl:string = 'https://sastramess.herokuapp.com/';
+
+  constructor(private httpClient: HttpClient) {}
+
+  private handleError(err: HttpErrorResponse){
+      console.error(err);
+      return throwError('Something bad has happened.Try again later.');
+  }
+
+  public putMenu(menu: Object): Observable<Response>{
+      var json = JSON.stringify(menu);
+      return this.httpClient.post(this.baseUrl + 'menu',json).pipe(
+          map(response => {
+              console.log(response);
+              return new Response(response);
+          }),
+          catchError(this.handleError)
+      );
+  }
+}
