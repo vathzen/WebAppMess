550634b0bafded43c42848e5f5f6a5e8a5e0167f
shrihari1999 <120004213@sastra.ac.in>
2019-08-27 00:08:24 +0530

    Added slot for snacks

--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -17,6 +17,7 @@ export class OrdersPage implements OnInit {
   public orders=[
     {mealname:'Breakfast', item1:null, item1_count:null, item2:null, item2_count:null, span:0},
     {mealname:'Lunch', item1:null, item1_count:null, item2:null, item2_count:null, span:0},
+    {mealname:'Snacks', item1:null, item1_count:null, item2:null, item2_count:null, span:0},
     {mealname:'Dinner', item1:null, item1_count:null, item2:null, item2_count:null, span:0}
   ];
   nullIndices = [];
@@ -41,16 +42,14 @@ export class OrdersPage implements OnInit {
   }
 
   updateTotal(){
-    var orders = ['Dosa',30,null,null,null,null,null,null,'Noodles',50,'Fried Rice',50];
-    var orders_count = [33,null,null,null,55,66];
-
+    var orders = ['Dosa',33,null,null,null,null,null,null,'Lemon Juice',30,'Samosa',20,'Noodles',50,'Fried Rice',50];
     var i=0;
     this.nullIndices.splice(0,this.nullIndices.length);
     this.orders.forEach(item => {//each iteration for each meal FOR GIVEN DATE
       item.span=0;
       if(orders[i]!=null){
         item.item1=orders[i].toString();
-        item.item1_count=Number(orders_count[i/2]);
+        item.item1_count=Number(orders[i+1]);
         item.span++;
       }
       else{
@@ -58,7 +57,7 @@ export class OrdersPage implements OnInit {
       }
       if(orders[i+2]!=null){
         item.item2=orders[i+2].toString();
-        item.item2_count=Number(orders_count[(i+2)/2]);
+        item.item2_count=Number(orders[i+3]);
         item.span++;
       }
       else{
@@ -67,78 +66,42 @@ export class OrdersPage implements OnInit {
       i+=4;
     });
   }
-
   updateTable(){//get new menu dictionary
     //Heading over, body starts
     this.codes.splice(0,this.codes.length); //delete prev day codes
     //get from db and push as {regnum:'regnum from db', code_array:[code1,code2,..code6] from db}
 
     var tempcodes = [
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"],
-    ["120005000","code1",null,null,null,"code2",null],
-    ["120005001",null,null,null,null,null,"code2"],
-    ["120005002","code1",null,null,null,"code2",null],
-    ["120005003",null,null,null,null,"code2",null],
-    ["120005004",null,null,null,null,null,"code2"]
+    ["120005000","code1",null,null,null,"code2",null,null,null],
+    ["120005001",null,null,null,null,null,"code2",null,null],
+    ["120005002","code1",null,null,null,"code2",null,null,null],
+    ["120005003",null,null,null,null,"code2",null,null,null],
+    ["120005004",null,null,null,null,null,"code2","code3",null],
+    ["120005000","code1",null,null,null,"code2",null,null,null],
+    ["120005001",null,null,null,null,null,"code2",null,null],
+    ["120005002","code1",null,null,null,"code2",null,null,null],
+    ["120005003",null,null,null,null,"code2",null,null,null],
+    ["120005004",null,null,null,null,null,"code2","code3",null],
+    ["120005000","code1",null,null,null,"code2",null,null,null],
+    ["120005001",null,null,null,null,null,"code2",null,null],
+    ["120005002","code1",null,null,null,"code2",null,null,null],
+    ["120005003",null,null,null,null,"code2",null,null,null],
+    ["120005004",null,null,null,null,null,"code2","code3",null],
+    ["120005000","code1",null,null,null,"code2",null,null,null],
+    ["120005001",null,null,null,null,null,"code2",null,null],
+    ["120005002","code1",null,null,null,"code2",null,null,null],
+    ["120005003",null,null,null,null,"code2",null,null,null],
+    ["120005004",null,null,null,null,null,"code2","code3",null],
+    ["120005000","code1",null,null,null,"code2",null,null,null],
+    ["120005001",null,null,null,null,null,"code2",null,null],
+    ["120005002","code1",null,null,null,"code2",null,null,null],
+    ["120005003",null,null,null,null,"code2",null,null,null],
+    ["120005004",null,null,null,null,null,"code2","code3",null],
+    ["120005000","code1",null,null,null,"code2",null,null,null],
+    ["120005001",null,null,null,null,null,"code2",null,null],
+    ["120005002","code1",null,null,null,"code2",null,null,null],
+    ["120005003",null,null,null,null,"code2",null,null,null],
+    ["120005004",null,null,null,null,null,"code2","code3",null],
   ];
     tempcodes.forEach(tempcode => {
       var regnum = tempcode[0];
